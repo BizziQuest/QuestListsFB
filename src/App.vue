b5671ece0151d374f29d7649e0113995b2d39fb0
Samuel C Tyler <skamansam@gmail.com>
2020-05-11 11:47:31 -0400

    create new app and import from questlists repo

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <p> {{message}} </p>
+    <v-btn class="primary" v-on:click="messageIs()"> what is message? </v-btn>
+    <v-alert
+      :value = "messageValue"
+      type = "success"
+    >
+      This is a success alert.
+    </v-alert>
+  </v-app>
+</template>
+
+
+<script>
+
+export default {
+  components: {},
+  data() {
+    return {
+      messageValue: false,
+      message: 'Vuetify is here',
+    };
+  },
+  methods: {
+    messageIs() {
+      this.messageValue = !this.messageValue;
+    },
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
