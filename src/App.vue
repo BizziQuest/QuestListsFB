c7f81c3572d157b28998960c160640b0ec65d381
Samuel C Tyler <skamansam@gmail.com>
2020-05-11 14:52:44 -0400

    add premade layout for keep

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,117 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+<div id="app">
+  <v-app id="inspire">
+    <v-app id="keep">
+      <v-app-bar
+        app
+        clipped-left
+        color="amber"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="title ml-3 mr-5">Google&nbsp;<span class="font-weight-light">Keep</span></span>
+        <v-text-field
+          solo-inverted
+          flat
+          hide-details
+          label="Search"
+          prepend-inner-icon="search"
+        ></v-text-field>
+  
+        <v-spacer></v-spacer>
+      </v-app-bar>
+  
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+        clipped
+        color="grey lighten-4"
+      >
+        <v-list
+          dense
+          class="grey lighten-4"
+        >
+          <template v-for="(item, i) in items">
+            <v-row
+              v-if="item.heading"
+              :key="i"
+              align="center"
+            >
+              <v-col cols="6">
+                <v-subheader v-if="item.heading">
+                  {{ item.heading }}
+                </v-subheader>
+              </v-col>
+              <v-col
+                cols="6"
+                class="text-right"
+              >
+                <v-btn
+                  small
+                  text
+                >edit</v-btn>
+              </v-col>
+            </v-row>
+            <v-divider
+              v-else-if="item.divider"
+              :key="i"
+              dark
+              class="my-4"
+            ></v-divider>
+            <v-list-item
+              v-else
+              :key="i"
+              link
+            >
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title class="grey--text">
+                  {{ item.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
+      </v-navigation-drawer>
+  
+      <v-content>
+        <v-container
+          fluid
+          class="grey lighten-4 fill-height"
+        >
+          <v-row
+            justify="center"
+            align="center"
+          >
+            <v-col class="shrink">
+              <v-tooltip right>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    :href="source"
+                    icon
+                    large
+                    target="_blank"
+                    v-on="on"
+                  >
+                    <v-icon large>mdi-code-tags</v-icon>
+                  </v-btn>
+                </template>
+                <span>Source</span>
+              </v-tooltip>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </v-app>
   </v-app>
+</div>
 </template>
-
+<style>
+#keep .v-navigation-drawer__border {
+display: none
+}
+</style>
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -52,9 +121,27 @@ export default {
   components: {
     HelloWorld,
   },
-
+  props: {
+    source: String,
+  },
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { icon: 'lightbulb_outline', text: 'Notes' },
+      { icon: 'touch_app', text: 'Reminders' },
+      { divider: true },
+      { heading: 'Labels' },
+      { icon: 'add', text: 'Create new label' },
+      { divider: true },
+      { icon: 'archive', text: 'Archive' },
+      { icon: 'delete', text: 'Trash' },
+      { divider: true },
+      { icon: 'settings', text: 'Settings' },
+      { icon: 'chat_bubble', text: 'Trash' },
+      { icon: 'help', text: 'Help' },
+      { icon: 'phonelink', text: 'App downloads' },
+      { icon: 'keyboard', text: 'Keyboard shortcuts' },
+    ],
   }),
 };
 </script>
