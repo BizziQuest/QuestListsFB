2771952c76fa6f88e5a6e2bd0e6ba0fba4fbb4ef
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-18 19:46:30 -0700

    added the signup user action mutation getter and state to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import { auth } from '../main'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null
+  },
+  getters: {
+    user: (state) => state.user
   },
   mutations: {
+    setUser(state, payload) {
+      //this one is thorwing an error 
+      //  state.user =[...payload]
+      // the following one will have issue if obj includes objs?
+      state.user = JSON.parse(JSON.stringify(payload))
+
+    }
   },
   actions: {
-  },
-  modules: {
-  },
+    async signupUser({ commit }, payload) {
+      try {
+        const cred = await auth.createUserWithEmailAndPassword(payload.email, payload.password)
+        console.log(cred)
+        commit("setUser", payload)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+  }
 });
