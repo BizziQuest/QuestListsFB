c5151cf4dd2a95ba8d47c1d7deab1ffdddb6bfdf
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-18 20:06:55 -0700

    created logout button component

---
+++ src/components/LogOut.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-btn outlined class="primary z-depth-0 darken" @click="logoutUser">Log out</v-btn>
+  </div>
+</template>
+<script>
+export default {
+  name: "LogOut",
+  methods: {
+    logoutUser() {
+      this.$store.dispatch("logoutUser");
+    }
+  }
+};
+</script>
