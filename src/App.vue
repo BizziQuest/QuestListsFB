411dc52cc7c56a529e1dec1da69ecd7d32d12ee2
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-24 20:44:29 -0700

    readded id insipre to App.vue'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-app>
+    <v-app id="inspire">
       <v-app>
         <top-menu-bar :drawer.sync="drawer"></top-menu-bar>
         <drawer-menu-bar :drawer="drawer"></drawer-menu-bar>
@@ -8,15 +8,8 @@
           <v-container fluid class="grey lighten-4 fill-height">
             <v-row justify="center" align="center">
               <v-col class="shrink">
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon large>mdi-code-tags</v-icon>
-                    </v-btn>
-                    <span style="color: red">{{drawer}}</span>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
+                <span style="color: red">{{ drawer }}</span>
+                <span style="color: red">{{ userInfo }}</span>
               </v-col>
             </v-row>
           </v-container>
@@ -27,14 +20,15 @@
 </template>
 
 <script>
-import TopMenuBarVue from "./components/TopMenuBar.vue";
-import DrawerMenuVue from "./components/DrawerMenu";
+import TopMenuBarVue from './components/TopMenuBar.vue';
+import DrawerMenuVue from './components/DrawerMenu.vue';
+
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
-    "top-menu-bar": TopMenuBarVue,
-    "drawer-menu-bar": DrawerMenuVue
+    'top-menu-bar': TopMenuBarVue,
+    'drawer-menu-bar': DrawerMenuVue
   },
   props: {
     source: String
@@ -43,25 +37,28 @@ export default {
     return {
       drawer: false,
       items: [
-        { icon: "lightbulb_outline", text: "Notes" },
-        { icon: "touch_app", text: "Reminders" },
+        { icon: 'lightbulb_outline', text: 'Notes' },
+        { icon: 'touch_app', text: 'Reminders' },
         { divider: true },
-        { heading: "Labels" },
-        { icon: "add", text: "Create new label" },
+        { heading: 'Labels' },
+        { icon: 'add', text: 'Create new label' },
         { divider: true },
-        { icon: "archive", text: "Archive" },
-        { icon: "delete", text: "Trash" },
+        { icon: 'archive', text: 'Archive' },
+        { icon: 'delete', text: 'Trash' },
         { divider: true },
-        { icon: "settings", text: "Settings" },
-        { icon: "chat_bubble", text: "Trash" },
-        { icon: "help", text: "Help" },
-        { icon: "phonelink", text: "App downloads" },
-        { icon: "keyboard", text: "Keyboard shortcuts" }
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Trash' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Keyboard shortcuts' }
       ]
     };
   },
-  methods: {
-   
-  }
+  computed: {
+    userInfo() {
+      return this.$store.getters.user;
+    }
+  },
+  methods: {}
 };
 </script>
