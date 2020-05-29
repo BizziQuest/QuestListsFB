7a56d596f1ceb0fda911b2a318c744387da72a69
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-29 12:02:17 -0700

    made swatch colors work

---
+++ src/components/CreateAList.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-list-item link v-on="on">
+          <v-list-item-action>
+            <v-icon>add</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="grey--text">create a list</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Create A List</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Title*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" md="12">
+                <v-text-field v-model="color">
+                  <template v-slot:append-outer>
+                    <v-menu>
+                      <template v-slot:activator="{ on }">
+                        <div :style="swatchStyle" v-on="on" />
+                      </template>
+                      <v-card>
+                        <v-card-text>
+                          <v-color-picker v-model="color" flat />
+                        </v-card-text>
+                      </v-card>
+                    </v-menu>
+                  </template>
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete :items="['Done', 'Not Done']" label="Status" multiple></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'CreateAList',
+  data() {
+    return {
+      color: '#1976D2FF',
+      menu: false,
+      dialog: false,
+    };
+  },
+  computed: {
+    swatchStyle() {
+      return {
+        backgroundColor: this.color,
+        cursor: 'pointer',
+        height: '30px',
+        width: '30px',
+        borderRadius: '4px',
+        transition: 'border-radius 200ms ease-in-out',
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
