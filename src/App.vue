898fff58c8a520ad7425dd18455f5feac098df65
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-02 18:25:22 -0700

    when there is not list, an user will encounter a message that asks Please add some stuff with sad emoji. when the user adds some lists, the message goes away and list or lists will show up 3 lists in a row

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <v-app id="keep">
+  <!-- id="keep" has no effect ? -->
+  <v-app>
     <!-- @updateDrawer="handleUpdate" -->
     <top-menu-bar :drawer.sync="drawer"></top-menu-bar>
     <drawer-menu-bar :drawer.sync="drawer"></drawer-menu-bar>
-      <v-container fluid class="grey lighten-4  fill-height">
-        <v-row >
-          <v-col col='4' md='4' justify="center" align="center">
-            <div style="color: red">Drawer: {{ drawer }}</div>
-            <!--div style="color: red">User: {{ userInfo }}</div-->
-            <template v-show="isUserUthenticated">
-            <div v-for="list in lists" :key="list.bgColor">
-                <v-card class=" mx-auto" max-width="350" tile :color="list.bgColor">
-                <v-list-item>
-                  <v-list-item-content>
-                    <v-list-item-title>Title: {{list.title}}</v-list-item-title>
-                    <ul v-for="item in list.items" :key="item.name">
-                      <li>Name: {{item.name}} | Status: {{item.state}}</li>
-                    </ul>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-card>
-            </div>
-            </template>
-          </v-col>
-        </v-row>
-      </v-container>
+    <v-container fluid class="grey lighten-4  fill-height">
+      <v-row>
+        <v-col v-if="lists.length < 1" col="12" md="12">
+          <!--div style="color: red">Drawer: {{ drawer }}</div>
+          <div style="color: red">User: {{ userInfo }}</div-->
+          <v-alert icon="mdi-emoticon-sad" type="info">
+            Please add some stuff
+          </v-alert>
+        </v-col>
+        <v-col v-else v-for="list in lists" :key="list.bgColor" col="12" md="4">
+            <v-card class=" mx-auto" min-height="250" max-width="350" tile :color="list.bgColor">
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Title: {{ list.title }}</v-list-item-title>
+                  <ul v-for="item in list.items" :key="item.name">
+                    <li>Name: {{ item.name }} | Status: {{ item.state }}</li>
+                  </ul>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -68,13 +70,14 @@ export default {
       return this.$store.getters.user;
     },
     isUserUthenticated() {
-      return (
-        this.$store.getters.user !== undefined
-        && this.$store.getters.user !== null
-      );
+      return this.$store.getters.user !== undefined && this.$store.getters.user !== null;
     },
     lists() {
-      return this.$store.getters.lists;
+      // if this.authenticated?
+      if (this.isUserUthenticated) {
+        return this.$store.getters.lists;
+      }
+      return [];
     },
   },
   methods: {
