2a04cd2c8bd0ff8f1e4a47f5693c5d214ff124bb
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-08 14:08:34 -0700

    v-btn shows the avatar on top right of top bar menu. Logout and Edit Info buttons were added to drop down menu. Clicking on Edit Info take an user to different rout /EditInfo

--- src/components/Menus/AvatarMenu.vue
+++ src/components/Menus/AvatarMenu.vue
@@ -1,33 +1,23 @@
 <template>
-    <div>
-        <v-menu center
-                offset-y
-                bottom
-                origin="center center"
-                transition="scale-transition">
-            <template v-slot:activator="{ on }">
-                <v-btn
-                    dark
-                    icon
-                    v-on="on"
-                >
-                    <v-avatar
-                        avatarStyle='Transparent'
-                        class="profile"
-                        color="grey"
-                        size="60"
-                        >
-                        <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
-                    </v-avatar>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item>
-                <LogOut></LogOut>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-    </div>
+  <div>
+    <v-menu center offset-y bottom origin="center center" transition="scale-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn dark icon v-on="on">
+          <v-avatar avatarStyle="Transparent" class="profile" color="grey" size="60">
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-btn outlined class="primary z-depth-0 darken pl-2" v-on:click="goToEditInfo">Edit Info</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <LogOut></LogOut>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script>
@@ -39,12 +29,14 @@ export default {
     LogOut,
   },
   data() {
-    return {
-    };
+    return {};
+  },
+  methods: {
+    goToEditInfo() {
+      this.$router.push('EditInfo');
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
