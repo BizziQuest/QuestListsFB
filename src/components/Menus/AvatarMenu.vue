3312ea2585e529a44d1d5aee0a77b80482e68e87
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-08 13:06:31 -0700

    created AvatarMenu component

---
+++ src/components/Menus/AvatarMenu.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-menu center
+                offset-y
+                bottom
+                origin="center center"
+                transition="scale-transition">
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    dark
+                    icon
+                    v-on="on"
+                >
+                    <v-avatar
+                        avatarStyle='Transparent'
+                        class="profile"
+                        color="grey"
+                        size="60"
+                        >
+                        <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+                    </v-avatar>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <LogOut></LogOut>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+    </div>
+</template>
+
+<script>
+import LogOut from '../LogOut.vue';
+
+export default {
+  name: 'AvatarMenu',
+  components: {
+    LogOut,
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
