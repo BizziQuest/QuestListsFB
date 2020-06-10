c5370212e9604063bab6033e586dbca337b44cd9
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-10 15:56:12 -0700

    got rid of the rules because first name and last name are optional, and still working on displayname null issue

--- src/components/EditInfo.vue
+++ src/components/EditInfo.vue
@@ -1,36 +1,48 @@
 <template>
   <div>
-    <h1 class="d-flex justify-center align-center mt-10">Edit Info</h1>
-    <v-form ref="editForm">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="firstname" :rules="nameRules" label="First name" clearable required></v-text-field>
-          </v-col>
+    <v-card class="mx-auto mt-10"  max-width="500" raised shaped>
+      <v-card-text>
+        <h1 class="d-flex justify-center align-center mt-10">Edit Info</h1>
+      </v-card-text>
+      <v-card-actions>
+        <v-form ref="editForm">
+          <v-container>
+            <v-row>
+              <v-col cols="12" md="4">
+                <v-text-field
+                  v-model="firstName"
+                  label="First name"
+                  clearable
+                  required
+                ></v-text-field>
+              </v-col>
 
-          <v-col cols="12" md="4">
-            <v-text-field v-model="lastname" :rules="nameRules" clearable label="Last name" required></v-text-field>
-          </v-col>
+              <v-col cols="12" md="4">
+                <v-text-field v-model="lastName"
+                  clearable
+                  label="Last name" required></v-text-field>
+              </v-col>
 
-          <v-col cols="12" md="4">
-            <v-text-field v-model="email" label="E-mail" disabled></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col class="d-flex align-center justify-center">
-            <v-btn class="success" color="darken-1" elevation="2" x-large rounded text
-            v-on:click="editForm">
-              Edit
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-row>
-          {{ user }}
-          <br />
-          {{ firebaseUser }}
-        </v-row>
-      </v-container>
-    </v-form>
+              <v-col cols="12" md="4">
+                <v-text-field v-model="email" label="E-mail" disabled></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="d-flex align-center justify-center">
+                <v-btn class="success" color="darken-1" elevation="2" x-large rounded text v-on:click="editForm">
+                  Edit
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              {{ user }}
+              <br />
+              displayname: {{ firebaseUser.displayName }}
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -42,15 +54,21 @@ export default {
   data() {
     return {
       valid: false,
-      firstname: '',
-      lastname: '',
-      nameRules: [
-        (v) => !!v || 'Name is required',
-      ],
+      firstName: '',
+      lastName: '',
+      nameRules: [(v) => !!v || 'Name is required'],
       email: '',
     };
   },
   mounted() {
+    console.log(this.firebaseUser.displayName);
+    if (this.firebaseUser.displayName) {
+      // eslint-disable-next-line prefer-destructuring
+      this.firstName = this.firebaseUser.displayName.split(' ')[0];
+      // eslint-disable-next-line prefer-destructuring
+      this.lastName = this.firebaseUser.displayName.split(' ')[1];
+    }
+
     this.email = this.firebaseUser.email;
   },
   computed: {
@@ -63,11 +81,10 @@ export default {
   },
   methods: {
     async editForm() {
-      if (this.$refs.editForm.validate()) {
-        const response = await auth.currentUser
-          .updateProfile({
-            displayName: this.firstname + this.lastname,
-          });
+      if (this.lastName.length !== 0 && this.firstName.length !== 0) {
+        const response = await auth.currentUser.updateProfile({
+          displayName: this.firstName.concat(' ', this.lastName),
+        });
         console.log(response, auth.currentUser);
       }
     },
