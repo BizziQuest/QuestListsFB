29e8d29af9fdbaa1eba42c518c77f582db34cce6
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-09 21:02:34 -0700

    added rules for the textfileds and data properties regarding any of them

--- src/components/EditInfo.vue
+++ src/components/EditInfo.vue
@@ -1,50 +1,37 @@
 <template>
-    <div>
-      <h1 class='d-flex justify-center align-center mt-10'>Edit Info</h1>
-     <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
-        </v-col>
+  <div>
+    <h1 class="d-flex justify-center align-center mt-10">Edit Info</h1>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="First name"
+              required
+            ></v-text-field>
+          </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
-        </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field>
+          </v-col>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
-    </div>
+          <v-col cols="12" md="4">
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="d-flex align-center justify-center">
+            <v-btn color="error" class="mr-4" @click="reset">
+              Reset Form
+            </v-btn></v-col
+          >
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -52,17 +39,26 @@ export default {
   name: 'EditInfo',
   data() {
     return {
-      items: [
-        { icon: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
-        { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-        { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-        { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        (v) => !!v || 'Name is required',
+        (v) => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+/.test(v) || 'E-mail must be valid',
       ],
     };
+  },
+  computed: {
+    user() {
+      return this.$store.getters.user;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
