42eb3de574e06b184b325fb021221ea00d6b201b
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-17 18:08:13 -0700

    made the cards more agreeable to the specification

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -15,33 +15,61 @@
           style="margin-bottom: 20px; margin-right: 5px; border-radius: 25px;"
         >
           <v-row>
-            <v-col md="6">
+            <v-col cols="6">
               <!--v-avatar max-width="300000px" class="ml-1" :size="idx === 0 ? '400px' : '200px'"  tile -->
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                       style="border-radius: 25px; margin-left: 5px"
-                       :style="idx === 0 ? 'max-width:450px' : 'max-width:425px' "></v-img>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                style=""
+                :style="idx === 0 ? 'max-width:450px' : 'max-width:425px'"
+              ></v-img>
+            </v-col>
+            <v-col md="6">
+              <v-row align="center">
+                <v-col cols="12">
+                  <v-card-text>
+                    <span class="display-3">{{ list.title }}</span>
+                    <div style="color: #4567ac;">Greyhound divisely hello coldly fonwderfully</div>
+                  </v-card-text>
                 </v-col>
-            <v-col>
-              <v-list-item>
-                <v-list-item-content style="color: black;">
-                  <v-list-item-title  class="headline mb-1">{{ list.title }}</v-list-item-title>
-                  <v-list-item-subtitle style="color: #4567ac;">
-                  Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-                  <ul v-for="item in [
-                    { name: 'List Item 1', state: 'Done' },
-                    { name: 'List Item 2', state: 'Not Done'}]" :key="item.name">
-                    <li> [{{ item.state }}] {{ item.name }} </li>
-                  </ul>
-                </v-list-item-content>
-              </v-list-item>
-                  <v-card-actions class="ml-4">
-                    <v-btn fab >
+              </v-row>
+              <v-row>
+                <v-list-item>
+                  <v-list-item-content style="color: black;">
+                    <v-card id="list" color="#37d7eb" shaped max-width="400">
+                      <ul
+                        v-for="item in [
+                          { name: 'List Item 1', state: 'Done' },
+                          { name: 'List Item 2', state: 'Not Done' },
+                          { name: 'List Item 3', state: 'Running' },
+                          { name: 'List Item 4', state: 'Hugging' },
+                          { name: 'List Item 5', state: 'Walking' },
+                          { name: 'List Item 6', state: 'Done' },
+                          { name: 'List Item 7', state: 'Not Done' },
+                          { name: 'List Item 8', state: 'Running' },
+                          { name: 'List Item 9', state: 'Hugging' },
+                          { name: 'List Item 10', state: 'Walking' }
+                        ]"
+                        :key="item.name"
+                      >
+                        <li>[{{ item.state }}] {{ item.name }}</li>
+                      </ul>
+                    </v-card>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-row>
+              <v-row class="d-flex align-self-end ml-4">
+                <v-col>
+                  <v-card-actions>
+                    <v-btn fab>
                       <v-icon>edit</v-icon>
                     </v-btn>
                     <v-btn fab>
                       <v-icon> pageview </v-icon>
                     </v-btn>
                   </v-card-actions>
+                </v-col>
+              </v-row>
+              <div></div>
             </v-col>
           </v-row>
         </v-card>
@@ -74,5 +102,20 @@ export default {
 <style lang="scss" scoped>
 ul {
   list-style-type: none;
+}
+
+#list {
+  height: 150px;
+  overflow: auto;
+}
+.v-responsive__content {
+    margin-left: 2px;
+    height: 514px;
+}
+.v-image{
+  margin-left: 10px;
+  border-radius: 25px;
+  margin-left: 5px;
+  height: 100%;
 }
 </style>
