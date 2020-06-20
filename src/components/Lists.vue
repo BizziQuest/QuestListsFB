fc3801477020be6ab6cc7ee47c69bc931054c85e
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-20 15:36:58 -0700

    card get sepreated from list and still works needs to be done

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -1,93 +1,32 @@
 <template>
   <v-container fluid>
     <v-row no-gutters>
-      <v-col v-if="lists.length < 1" col="12" md="12">
-        <!--div style="color: red">Drawer: {{ drawer }}</div>
-          <div style="color: red">User: {{ userInfo }}</div-->
-        <v-alert icon="mdi-emoticon-sad" type="info">
-          Please add some stuff
-        </v-alert>
+      <v-col v-if="lists.length < 1" col="12" xs="12" md="12">
+        <v-alert icon="mdi-emoticon-sad" type="info">Please add some stuff</v-alert>
       </v-col>
-      <v-col v-else v-for="(list, idx) in lists" :key="list.title" :cols="idx === 0 ? 12 : 6">
-        <v-card
-          min-height="200"
-          :color="list.bgColor"
-          style="margin-bottom: 20px; margin-right: 5px; border-radius: 25px;"
-        >
-          <v-row>
-            <v-col cols="6">
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                style=""
-                :style="idx === 0 ? 'max-width:450px' : 'max-width:425px'"
-              ></v-img>
-            </v-col>
-            <v-col md="6">
-              <v-row align="center">
-                <v-col cols="12">
-                  <v-card-text>
-                    <span class="display-3">{{ list.title }}</span>
-                    <div style="color: #4567ac;">Greyhound divisely hello coldly fonwderfully</div>
-                  </v-card-text>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-list-item>
-                  <v-list-item-content style="color: black;">
-                    <v-card class="list" color="#37d7eb" shaped max-width="400">
-                      <ul
-                        v-for="item in [
-                          { name: 'List Item 1', state: 'Done' },
-                          { name: 'List Item 2', state: 'Not Done' },
-                          { name: 'List Item 3', state: 'Running' },
-                          { name: 'List Item 4', state: 'Hugging' },
-                        ]"
-                        :key="item.name"
-                      >
-                        <li>[{{ item.state }}] {{ item.name }}</li>
-                      </ul>
-                      <span class="ma-5">more...</span>
-                    </v-card>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-row>
-              <v-row class="d-flex align-self-end ml-4">
-                <v-col>
-                  <v-card-actions>
-                    <v-btn fab>
-                      <v-icon>edit</v-icon>
-                    </v-btn>
-                    <v-btn fab>
-                      <v-icon> pageview </v-icon>
-                    </v-btn>
-                  </v-card-actions>
-                </v-col>
-              </v-row>
-              <div></div>
-            </v-col>
-          </v-row>
-        </v-card>
+      <!--v-col v-else v-for="(list, idx) in lists" :key="list.title" :cols="idx % 2 !== 0 ? 12 : 6"-->
+      <v-col v-else v-for="list in lists" :key="list.title" cols="12" xs="12" sm="6" md="4">
+        <!--ListCard :list="list" :large="idx % 2 !== 0"></ListCard-->
+        <ListCard :list="list"></ListCard>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import ListCard from './ListCard.vue';
+
 export default {
   name: 'Lists',
+  components: {
+    ListCard,
+  },
   computed: {
     userInfo() {
       return this.$store.getters.user;
     },
-    isUserUthenticated() {
-      return this.$store.getters.user !== undefined && this.$store.getters.user !== null;
-    },
     lists() {
-      // if this.authenticated?
-      if (this.isUserUthenticated) {
-        return this.$store.getters.lists;
-      }
-      return [];
+      return this.$store.getters.lists;
     },
   },
 };
@@ -96,20 +35,20 @@ export default {
 <style lang="scss" scoped>
 ul {
   list-style-type: none;
+  li {
+    line-height: 30px;
+  }
 }
-
 .list {
-  height: 150px;
+  height: 99px;
   overflow: auto;
 }
-.v-responsive__content {
-    margin-left: 2px;
-    height: 514px;
-}
-.v-image{
+// .v-responsive__content {
+//   margin-left: 20px;
+//   height: 50px;
+// }
+.v-image {
   margin-left: 10px;
   border-radius: 25px;
-  margin-left: 5px;
-  height: 100%;
-}
+  }
 </style>
