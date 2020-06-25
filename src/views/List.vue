18ee39b8063f25856438c12e6d07685e289f6801
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-24 22:13:42 -0700

    added v-data-table to show the list items, and added New Item button to add new item to the list

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,12 +2,38 @@
   <div>
     <h1>List</h1>
     <p>{{list}}</p>
+    <p>{{list.listItems}}</p>
+    <v-data-table
+      :headers="headers"
+      :items="list.listItems"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+    <v-btn v-on:click="addNewItem">+ New Item</v-btn>
   </div>
 </template>
 <script>
 export default {
   name: 'List',
   props: ['title'],
+  data() {
+    return {
+      headers: [
+        {
+          text: 'item',
+          align: 'start',
+          sortable: true,
+          value: 'text',
+        },
+        { text: 'state', value: 'state' },
+      ],
+    };
+  },
+  methods: {
+    addNewItem() {
+      this.list.listItems.push({ text: 'item3', state: 'Done' });
+    },
+  },
   computed: {
     list() {
       return this.$store.getters.list(this.title);
