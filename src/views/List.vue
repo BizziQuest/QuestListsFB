7729cd3024b8879449d1c22675df51060fe5794a
kickbuttowski80 <pazpaz25@gmail.com>
2020-06-24 21:50:39 -0700

    List component was made

---
+++ src/views/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>List</h1>
+    <p>{{list}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'List',
+  props: ['title'],
+  computed: {
+    list() {
+      return this.$store.getters.list(this.title);
+    },
+  },
+};
+</script>
