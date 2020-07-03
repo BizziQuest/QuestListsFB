f20b5e6dd7c7cecd04c1bbcbf1c2e9f6c74c60ec
Samuel C Tyler <skamansam@gmail.com>
2020-07-03 01:07:59 -0400

    remove rally preset

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,28 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-// import { preset as rallyPreset } from 'vue-cli-plugin-vuetify-preset-rally/preset';
 import '@mdi/font/css/materialdesignicons.css'; // for the default material design icons
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset';
-// import { VuetifyPreset } from 'vuetify/types/services/presets';
 
 Vue.use(Vuetify);
-
 export default new Vuetify({
-  preset,
   theme: {
-    dark: true,
+    default: 'light',
     options: {
       customProperties: true,
     },
-    icons: {
-      iconfont: 'mdi',
-    },
     themes: {
       light: {
-        primary: '#fff176',
-        secondary: '#ab47bc',
-        accent: '#cabf45',
+        primary: '#FFF176',
+        secondary: '#AB47BC',
+        accent: '#CABF45',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
