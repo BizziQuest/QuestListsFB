ba6c31b052e4decb0cfd923cfe2158023541822b
Samuel C Tyler <skamansam@gmail.com>
2020-07-03 01:06:24 -0400

    add pwa info for vue

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,17 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+
   devServer: {
     disableHostCheck: true,
   },
+
+  pwa: {
+    name: 'Questlists',
+    themeColor: '#ab47bc',
+    msTileColor: '#ab47bc',
+    manifestOptions: {
+      background_color: '#fff176'
+    }
+  }
 };
