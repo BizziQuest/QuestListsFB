8bfabcf16fee979b63d266175bc761cab54bbdbf
kickbuttowski80 <pazpaz25@gmail.com>
2020-07-06 10:59:10 -0700

    used mapGetters and deleted un-used code snippet

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,10 +2,10 @@
   <div>
     <br>
     <h1>List</h1>
-    <p>{{list(title)}}</p>
-    <p>{{list(title).listItems}}</p>
+    <p>{{theList}}</p>
+    <p>{{theList.listItems}}</p>
     <ol style="list-style-type:none;">
-      <li v-for="(item,i) in listItems" :key="`${item.text}${i}`">
+      <li v-for="(item,i) in theList.listItems" :key="`${item.text}${i}`">
         <list-item :listItem="item"/>
       </li>
     </ol>
@@ -35,21 +35,14 @@ export default {
       console.log(this.$refs[ref]);
       this.$refs[ref][0].outlined = true;
     },
-    // not usage in this branch
-    // itemChanged($event) {
-    //   this.saveToServer(this.items);
-    // },
   },
   computed: {
     ...mapGetters(['list']),
-    listItems() {
-      return this.list.listItems;
+    theList() {
+      return this.list(this.title);
     },
-    // list() {
-    //   return this.$store.getters.list(this.title);
-    // },
-    allItemsStates() {
-      return this.$store.getters.itemStates;
+    listItems() {
+      return this.theList.listItems;
     },
   },
 };
