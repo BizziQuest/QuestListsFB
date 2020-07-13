480d6a2416c665aac8a751ac437bd9119d059235
kickbuttowski80 <pazpaz25@gmail.com>
2020-07-13 16:08:42 -0700

    got rid of unccessary props and unwanted code, and changed logic of going thorugh the icons

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -2,7 +2,7 @@
   <div class="list-item-view">
     <template>
       <v-container>
-         <v-row>
+        <v-row>
           <v-col col="12" md="6">
             <v-text-field
               style="margin-bottom:20px;"
@@ -11,28 +11,16 @@
               :outlined="isActive"
               @click="activate"
               @blur="deactivate "
-              >{{ listItem.text }}</v-text-field
-            >
+            >{{ listItem.text }}</v-text-field>
           </v-col>
           <v-col col="12" md="6">
-            <!--v-select
-              style="margin-bottom:20px;"
-              :items="itemStates"
-              :label="listItem.state"
-              @change = "listItem.state = $event"
-              :outlined="isActive"
-              @click="activate"
-              @blur="deactivate "
-            ></v-select-->
             <v-icon
-               style="margin-bottom:20px;"
+              style="margin-bottom:20px;"
               :outlined="isActive"
               @click="activate"
-              @blur="deactivate ">
-              {{listItem.state}}
-            </v-icon>
+              @blur="deactivate"
+            >{{listItem.state}}</v-icon>
           </v-col>
-          {{listStates[index]}}--{{index}}
         </v-row>
       </v-container>
     </template>
@@ -47,35 +35,37 @@ export default {
     listItem: {
       type: Object,
       default: () => ({}),
-      description: 'The list item was passed from List with more info like array of'
-      + 'objects which each obj includes text and state key values',
-    },
-    index: {
-      type: Number,
-      description: 'it is the index of list item',
+      description: 'an obj includes text and state key values',
     },
     listStates: {
       type: Array,
       description: 'all the item and states',
     },
   },
-  data: () => ({
-    isActive: false,
-  }),
+  data() {
+    return {
+      isActive: false,
+    };
+  },
   methods: {
     deactivate() {
       this.isOutlined = false;
     },
     activate() {
-      // this.isOutlined = true;
-      console.log('I am clicked', this.listStates[this.index + 1]);
-      this.listItem.state = this.listStates[this.index + 1].state;
+      this.isOutlined = true;
+      const currentState = this.listItem.state;
+      let stateIndex = this.itemStates.findIndex((state) => state === currentState);
+      if (stateIndex === this.itemStates.length - 1) {
+        this.listItem.state = this.itemStates[stateIndex - (this.itemStates.length - 1)];
+        stateIndex -= 1;
+      } else {
+        this.listItem.state = this.itemStates[stateIndex + 1];
+        this.localIndex += 1;
+      }
     },
   },
   computed: {
-    ...mapGetters([
-      'itemStates',
-    ]),
+    ...mapGetters(['itemStates']),
   },
 };
 </script>
