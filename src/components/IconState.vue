ebb2306c100fdb7fd7731b7de70f99ffca672202
kickbuttowski80 <pazpaz25@gmail.com>
2020-07-22 14:58:28 -0700

    changed the prop naming to be more readable; reseting the localIcon to the passedIcon prop when the chang needs to be canceled;

--- src/components/IconState.vue
+++ src/components/IconState.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="icon-state">
-    <v-icon color="primary" dark @click.stop="dialog = true">{{ item }}</v-icon>
+    <v-icon color="primary" dark @click.stop="dialog = true">{{ localIcon }}</v-icon>
     <v-dialog v-model="dialog" max-width="400">
       <v-card>
         <v-card-title class="headline">Please choose your Icon?</v-card-title>
@@ -10,14 +10,14 @@
             https://material.io/resources/icons/ or https://cdn.materialdesignicons.com/5.3.45/.
           </v-row>
           <v-row>
-            <v-icon>{{iconText}}</v-icon>
-            <v-text-field v-model="iconText"></v-text-field>
+            <v-icon>{{localIcon}}</v-icon>
+            <v-text-field v-model="localIcon"></v-text-field>
           </v-row>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false">Nah...</v-btn>
-          <v-btn color="green darken-1" text @click="choose">Choose</v-btn>
+          <v-btn color="green darken-1" text @click="close">Nah...</v-btn>
+          <v-btn color="green darken-1" text @click="chooseIcon">Choose</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -26,16 +26,21 @@
 
 <script>
 export default {
-  props: ['item'],
+  props: ['passedIcon'],
   data() {
     return {
-      iconText: this.item,
+      localIcon: this.passedIcon,
       dialog: false,
     };
   },
   methods: {
-    choose() {
-      this.$emit('myEvent', this.iconText);
+    chooseIcon() {
+      this.$emit('update:icon', this.localIcon);
+      this.dialog = false;
+    },
+    close() {
+      this.dialog = false;
+      this.localIcon = this.passedIcon;
     },
   },
 };
