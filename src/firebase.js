942c091220b031fa8010d2cb5252842a87e3ba21
Samuel C Tyler <skamansam@gmail.com>
2020-08-13 00:32:55 -0400

    add firebase analytics

--- src/firebase.js
+++ src/firebase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // firebase utils
 const fbApp = firebase.initializeApp(firebaseConfig);
-const fbAnalytics = firebase.analytics();
+const fbAnalytics = process.env.NODE_ENV !== 'test' ? firebase.analytics() : null;
 const auth = fbApp.auth();
 const db = fbApp.firestore();
 const { currentUser } = auth;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-import store from './store';// eslint-disable-line import/no-cycle
+import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import { auth } from './firebase';
