a12af94cdef63471623c4a654ef94663bb8dd951
kickbuttowski80 <pazpaz25@gmail.com>
2020-08-20 19:17:58 -0700

    implemented drag and drop for computer and mobile, and move the all the logic to a mixin for later use

--- src/components/StatesEditor.vue
+++ src/components/StatesEditor.vue
@@ -1,14 +1,21 @@
-<template>
+  <template>
   <v-container fluid>
     <span>Possible Item States:</span>
-    <div class="drop-zone"
-         @drop="onDrop"
-         @dragover="allowDrop($event)">
+    <div
+      id="drop-zone"
+      @drop="onDrop"
+      @dragover="allowDrop"
+      @touchmove="moveTouch"
+      @touchend="endTouch"
+    >
       <span
         v-for="(item,index) in items"
-        :key="`${item.text}${index}`"
+        :key="`${item.text}${index}${numForceRedraws}`"
         :data-index="index"
+        :id="index"
         @dragstart="startDrag"
+        @touchstart="startTouch"
+        class="item-row"
       >
         <list-state :item="item"
                     draggable="true"
@@ -19,9 +26,11 @@
 </template>
 <script>
 import ListState from './ListState.vue';
+import DragNDrop from '../mixins/DragNDrop.vue';
 
 export default {
   name: 'StatesEditor',
+  mixins: [DragNDrop],
   components: {
     ListState,
   },
@@ -35,8 +44,10 @@ export default {
   data() {
     return {
       items: this.stateItems,
+      rowItems: [],
     };
   },
+
   methods: {
     ensureNewState(index, state) {
       const lastStateIndex = this.items.length - 1;
@@ -46,89 +57,24 @@ export default {
         }
       }
     },
-    startDrag: ($event) => {
-      const sourceIndex = parseInt($event.target
-        .closest('[data-index]')
-        .getAttribute('data-index'), 10);
-      console.log('evt index vs source index', $event, sourceIndex);
-      const event = $event;
-      event.dataTransfer.dropEffect = 'move';
-      event.dataTransfer.effectAllowed = 'move';
-      event.dataTransfer.setData('sourceIndex', sourceIndex);
-    },
-    onDrop($event) {
-      // DEFINE: a pivot is a point at which a list is broken up. This method
-      //         has 2 pivots: the index for the item we are moving and the target index
-      // a,c,d,b,e,f => a,b,c,d,e,f
-      // moving (sourceTarget): b @ 3 => b @ 1
-      // dropTarget: a@0  # NOTE: droptarget comes before sourceTarget, so our first pivot is there
-      // [0..dropTarget], [dropTarget..sourceTarget-1], [sourceTarget+1..end]
-      // [0..droptarget] + [sourceTarget] + [dropTarget..sourcetarget-1] + [sourceTarget+1..end]
-
-      // list.reduce([], () => {
-      //   // there may be another way to reduce() this list: build a new list with the item inserted
-      //   // check each item to see if the insertion is needed and insert it.
-      // });
-
-      const array = [...this.items];
-      const sourceIndex = parseInt($event.dataTransfer.getData('sourceIndex'), 10);
-      console.log('type of sourceIndex', sourceIndex);
-      console.log('element is', document.getElementById(sourceIndex));
-      const targetIndex = parseInt($event.target
-        .closest('[data-index]')
-        .getAttribute('data-index'), 10);
-      console.log('Source Item (dragged item): ', sourceIndex, array[sourceIndex]);
-      console.log('Target Item (dropped onto): ', targetIndex, array[targetIndex]);
-
-      // console.log('type of sourceIndex', typeof sourceIndex);
-      // console.log('type of targetIndex', typeof targetIndex);
-      // console.log('sourceIndex === targetIndex + 1', sourceIndex === targetIndex + 1);
-      if (sourceIndex === targetIndex || sourceIndex === targetIndex + 1) {
-        console.log('No motion:', array);
-        $event.preventDefault();
-        return;
-      }
-      if (sourceIndex > targetIndex) {
-        const itemsBeforeAndIncludingTarget = array.slice(0, targetIndex + 1);
-        // console.log('itemsBeforeAndIncludingTarget', itemsBeforeAndIncludingTarget, 0, targetIndex + 1);
-
-        const itemsTargetToSourceNotIncludingSource = array.slice(targetIndex + 1, sourceIndex);
-        // console.log('itemsTargetToSourceNotIncludingSource', itemsTargetToSourceNotIncludingSource);
-
-        const itemsAfterSource = array.slice(sourceIndex + 1);
-        this.items = itemsBeforeAndIncludingTarget.concat(
-          array[sourceIndex],
-          itemsTargetToSourceNotIncludingSource,
-          itemsAfterSource,
-        );
-      } else {
-        const itemsBeforeAndNOTIncludingSource = array.slice(0, sourceIndex);
-        console.log('itemsBeforeAndNOTIncludingSource', itemsBeforeAndNOTIncludingSource, 0, sourceIndex);
-
-        const itemsAfterSourceToTargetIncludingTarget = array.slice(sourceIndex + 1, targetIndex + 1);
-        console.log('itemsAfterSourceToTargetIncludingTarget', itemsAfterSourceToTargetIncludingTarget);
-
-        const itemsAfterTarget = array.slice(targetIndex + 1);
-        console.log('itemsAfterTarget', itemsAfterTarget);
-        this.items = itemsBeforeAndNOTIncludingSource.concat(
-          itemsAfterSourceToTargetIncludingTarget,
-          array[sourceIndex],
-          itemsAfterTarget,
-        );
-      }
-
-      $event.preventDefault();
-    },
-    allowDrop($event) {
-      $event.preventDefault();
-    },
   },
 };
 </script>
-<style scoped>
-#box {
-  border: 1px solid blueviolet;
-  min-height: 100px;
+<style lang="scss" scoped>
+#drop-zone { 
+  min-height: 300px;
+  min-width: 200px;
+  width: 275px;
   height: auto;
+  margin: 0;
+  padding: 0; 
+}
+.start-drag {
+  opacity: 0.1;
+  border: 11px solid red;
+}
+.end-drag {
+  opacity: 1;
+  border: 11px solid blue;
 }
 </style>
