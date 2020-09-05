b9b3d05986d52762da36b002df84a3e6afb57b8e
Samuel C Tyler <skamansam@gmail.com>
2020-09-05 15:44:00 -0400

    add some tests!

---
+++ tests/e2e/specs/app-header-tests.js
@@ -0,0 +1,27 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('App shows app name', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/');
+    cy.contains('header', 'Quest Lists');
+  });
+});
+
+describe('App shows app name', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/');
+    cy.contains('header button', 'LOG IN / SIGN UP');
+  });
+});
+
+describe('Message when no lists are available', () => {
+  it('Visits the app root url', () => {
+    cy.server();
+    cy.route('http://localhost:8888/*').as('firebaseRequest');
+    cy.visit('/');
+    cy.wait('@firebaseRequest', {timeout: 30000});
+    cy.contains('.v-alert', 'There are no lists yet. Please add a list.');
+  });
+});
+
+
