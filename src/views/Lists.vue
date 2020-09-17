4f35b801f21ba22fc09beafb39cd4217792ecb99
Samuel C Tyler <skamansam@gmail.com>
2020-09-17 13:42:38 -0400

    add skeleton loader for lists

--- src/views/Lists.vue
+++ src/views/Lists.vue
@@ -1,21 +1,24 @@
 <template>
   <v-container fluid>
-      <v-row no-gutters>
-        <v-col v-if="lists && lists.length < 1" col="12" md="6" class='mt-5'>
-          <v-alert prominent icon="mdi-emoticon-sad" type="info">
-            Please add some stuff
-          </v-alert>
-        </v-col>
-        <v-col v-if="lists === null" col="12" md="12" class='mt-5'>
-          <v-alert prominent icon="mdi-emoticon-happy" type="info">
-            Loading...
-          </v-alert>
-        </v-col>
-        <v-col v-else v-for="list in lists" :key="list.id" col="12" md="4">
-          <ListCard :list="list"></ListCard>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row>
+      <v-col v-if="lists && lists.length < 1" col="12" md="6" class='mt-5'>
+        <v-alert prominent icon="mdi-emoticon-sad" type="info">
+          Please add some stuff
+        </v-alert>
+      </v-col>
+      <v-col v-if="lists === null" col="12" md="12" class='mt-5'>
+        <v-skeleton-loader v-for="i in 6" :key="i"
+          transition="fade"
+          class="mx-auto"
+          max-width="800"
+          type="list-item-avatar, divider, list-item-three-line, card-heading, image, actions"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col v-else v-for="list in lists" :key="list.id" col="12" md="4">
+        <ListCard :list="list"></ListCard>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
