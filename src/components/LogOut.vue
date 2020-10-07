ecdcfa64e4881249d1a552e9e2c0a49b0660d114
kickbuttowski80 <pazpaz25@gmail.com>
2020-10-06 18:14:11 -0700

    fixed logout icon

--- src/components/LogOut.vue
+++ src/components/LogOut.vue
@@ -3,7 +3,7 @@
     <v-tooltip left>
       <template v-slot:activator="{ on, attrs }">
         <v-btn class="primary--text" text v-bind="attrs" v-on="on" v-on:click="logoutUser">
-          <v-icon>exit_to_app</v-icon>
+          <v-icon color="secondary">exit_to_app</v-icon>
         </v-btn>
       </template>
       <span>Log Out</span>
