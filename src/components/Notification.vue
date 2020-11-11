376f0e4ee451b0a10475100b77009f0cd2181956
kickbuttowski80 <pazpaz25@gmail.com>
2020-11-10 21:29:25 -0800

    add notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-snackbar v-model="showSnackbar" top :color='`${message.type}`'>
+    {{ message.text }}
+     <template v-slot:action="{ attrs }">
+      <v-btn text v-bind="attrs" @click="showSnackbar = false">
+        X
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data: () => ({
+    showSnackbar: false,
+  }),
+  computed: {
+    ...mapState({
+      message(state) {
+        if (!state.message || !state.message.text) return '';
+        this.showSnackbar = true;
+        return state.message;
+      },
+    }),
+  },
+};
+</script>
+<style scoped>
+</style>
