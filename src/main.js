bb01656bdf48b269c89a806e8e49b24f033a9da2
Samuel C Tyler <skamansam@gmail.com>
2020-11-29 01:41:10 -0500

    add i18n

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
 // we can use this if we need, but the reply theme preset uses a different font
 // import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import { auth } from './firebase';
+
+import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
@@ -29,5 +32,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -66,4 +66,13 @@ module.exports = {
       ],
     },
   },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
 };
