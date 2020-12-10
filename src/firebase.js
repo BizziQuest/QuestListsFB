f58175e278fe60321f81a789d7f18863fdcbfc5a
Samuel C Tyler <skamansam@gmail.com>
2020-12-10 15:14:48 -0500

    add env to ci output

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ require('dotenv').config();
 // firebase init goes here
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  measurementId: process.env.VUE_APP_FIREBASE_APP_ID,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
 // firebase utils
