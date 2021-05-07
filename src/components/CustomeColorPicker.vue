df6fdea52c3b7a5d427f927f6a35127a6b16faf1
KickButtowski80 <pazpaz25@gmail.com>
2021-05-06 17:22:57 -0700

    added sliders and canvas checkboxes

--- src/components/CustomeColorPicker.vue
+++ src/components/CustomeColorPicker.vue
@@ -1,11 +1,44 @@
 <template>
-  <v-color-picker flat :value="value" @input="$emit('input', $event)" />
+  <div>
+    <v-color-picker flat :value="value" @input="$emit('input', $event)"
+     :hide-canvas="isCanvas"
+     :hide-sliders="isSliders"/>
+    <v-checkbox
+    label= "Canvas"
+    v-model="havingCanvas"
+    @click.stop=""></v-checkbox>
+    <v-checkbox
+    label="Sliders"
+    v-model="havingSliders"
+    @click.stop=""></v-checkbox>
+  </div>
 </template>
 <script>
 export default {
   props: ['value'],
   data() {
-    return {};
+    return {
+      isCanvas: true,
+      isSliders: true,
+    };
+  },
+  computed: {
+    havingCanvas: {
+      get() {
+        return !this.isCanvas;
+      },
+      set(newValue) {
+        this.isCanvas = !newValue;
+      },
+    },
+    havingSliders: {
+      get() {
+        return !this.isSliders;
+      },
+      set(newValue) {
+        this.isSliders = !newValue;
+      },
+    },
   },
 };
 </script>
