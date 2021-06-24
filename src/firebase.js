a05c546581d06e651d50a6dff5f22cdd7c643d31
Samuel C Tyler <skamansam@gmail.com>
2021-06-24 00:34:25 -0400

    remove unnecessary firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,17 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  measurementId: process.env.VUE_APP_FIREBASE_APP_ID,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // measurementId: process.env.VUE_APP_FIREBASE_APP_ID,
+  // messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  // storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
 };
 
+if (process.env.NODE_ENV !== 'development') {
+  firebaseConfig.measurementId = process.env.VUE_APP_FIREBASE_APP_ID;
+  firebaseConfig.messagingSenderId = process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID;
+  firebaseConfig.storageBucket = process.env.VUE_APP_FIREBASE_STORAGE_BUCKET;
+}
 // firebase utils
 const fbApp = firebase.initializeApp(firebaseConfig);
 const nodeEnv = process.env.NODE_ENV;
