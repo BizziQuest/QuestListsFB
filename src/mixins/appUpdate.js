03595b4b337a06455f7fe7491d4a5c254c652e9a
kickbuttowski80 <pazpaz25@gmail.com>
2021-07-08 20:46:29 -0700

    [finishes #110] Update app specs to reflect new app update behavior

--- src/mixins/appUpdate.js
+++ src/mixins/appUpdate.js
@@ -9,28 +9,22 @@ export default {
   },
 
   created() {
-    // Listen for our custom event from the SW registration
     document.addEventListener('swUpdated', this.updateAvailable, { once: true });
 
     // Prevent multiple refreshes
     navigator.serviceWorker?.addEventListener('controllerchange', () => {
       if (this.refreshing) return;
       this.refreshing = true;
-      // Here the actual reload of the page occurs
       window.location.reload();
     });
   },
 
   methods: {
-    // Store the SW registration so we can send it a message
-    // We use `updateExists` to control whatever alert, toast, dialog, etc we want to use
-    // To alert the user there is an update they need to refresh for
     updateAvailable(event) {
-      this.registration = event.detail;
+      this.registration = event.detail; // store this so we can use it if needed
       this.updateExists = true;
     },
 
-    // Called when the user accepts the update
     refreshApp() {
       this.updateExists = false;
       // Make sure we only send a 'skip waiting' message if the SW is waiting
--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,17 +1,11 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount, mount } from '@vue/test-utils';
 import App from '@/App.vue';
-import Vue from 'vue';
 import Vuex from 'vuex';
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
 import routes from '@/router/routes';
-import { saveListItems, getListBySlug, getListItems } from '../../src/firebase';
+
 jest.mock('../../src/firebase.js');
-// import toHaveBeenWarnedInit from '../toHaveBeenWarned';
-
-// toHaveBeenWarnedInit();
-
-// Vue.use(vuetify);
 
 let wrapper = null;
 
@@ -22,44 +16,42 @@ localVue.use(VueRouter);
 localVue.use(Vuex);
 localVue.use(Vuetify);
 
-const elem = document.createElement('div');
-if (document.body) {
-  document.body.appendChild(elem);
-}
-beforeEach(() => {
-  const state = {
-    messages: [],
-    currentUser: {
-      avatar: '/img/unknown_user.svg', // when we set the user in the store, we default to this.
-      displayName: null,
-      email: 'tersterson3@test.com',
-      emailVerified: false,
-      uid: 'UUID123456',
-      useGravatar: false,
-    },
-    globalPreferences: {},
-  };
-  const localStore = new Vuex.Store({
-    getters: { getGlobalPreferences: (s) => s.globalPreferences },
-    state,
-    actions: { fetchLists: jest.fn() },
+describe('App.vue component', () => {
+  beforeEach(() => {
+    const fakeBodyElement = document.createElement('div');
+    if (document.body) {
+      document.body.appendChild(fakeBodyElement);
+    }
+    const state = {
+      messages: [],
+      currentUser: {
+        avatar: '/img/unknown_user.svg', // when we set the user in the store, we default to this.
+        displayName: null,
+        email: 'tersterson3@test.com',
+        emailVerified: false,
+        uid: 'UUID123456',
+        useGravatar: false,
+      },
+      globalPreferences: {},
+    };
+    const localStore = new Vuex.Store({
+      getters: { getGlobalPreferences: (s) => s.globalPreferences },
+      state,
+      actions: { fetchLists: jest.fn() },
+    });
+    wrapper = shallowMount(App, {
+      localVue,
+      vuetify,
+      router,
+      store: localStore,
+      propsData: {},
+      attachTo: fakeBodyElement,
+    });
+  });
+  afterEach(() => {
+    wrapper.destroy();
   });
 
-  wrapper = shallowMount(App, {
-    localVue,
-    vuetify,
-    router,
-    store: localStore,
-    propsData: { },
-    attachTo: elem,
-  });
-});
-
-// afterEach(() => {
-//   wrapper.destroy();
-// });
-
-describe('App.vue', () => {
   it('renders a single v-app element', () => {
     expect(wrapper.findAll('v-app-stub').length).toBe(1);
   });
@@ -72,13 +64,49 @@ describe('App.vue', () => {
   it('renders the router view', () => {
     expect(wrapper.findAll('router-view-stub').length).toBe(1);
   });
-  it('shows a notification when the app has been updated', async () => {
-    expect(wrapper.find('[test-update-notification]')).toBeFalsy();
-    await wrapper.vm.trigger('swUpdate', {});
-    expect(wrapper.find('[test-update-notification]').exists()).toBe(true);
-  });
 });
 
-/// NOTE:
-// mock firebase
-// mock firebase.auth.currentUser to return fake object with user's properties, including emailVerified
+describe('App.vue response to service worker update', () => {
+  beforeEach(() => {
+    const fakeBodyElement = document.createElement('div');
+    if (document.body) {
+      document.body.appendChild(fakeBodyElement);
+    }
+    const state = {
+      messages: [],
+      currentUser: {
+        avatar: '/img/unknown_user.svg', // when we set the user in the store, we default to this.
+        displayName: null,
+        email: 'tersterson3@test.com',
+        emailVerified: false,
+        uid: 'UUID123456',
+        useGravatar: false,
+      },
+      globalPreferences: {},
+    };
+    const localStore = new Vuex.Store({
+      getters: { getGlobalPreferences: (s) => s.globalPreferences },
+      state,
+      actions: { fetchLists: jest.fn() },
+    });
+    wrapper = mount(App, {
+      localVue,
+      vuetify,
+      router,
+      store: localStore,
+      propsData: {},
+      attachTo: fakeBodyElement,
+    });
+  });
+  afterEach(() => {
+    wrapper.destroy();
+  });
+  it('shows a notification when the app has been updated', async () => {
+    expect(wrapper.find('[test-update-notification]').vm.value).toBe(false);
+    await wrapper.trigger('swUpdated', { detail: 'Finished Loading' });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.updateExists).toBe(true);
+    expect(wrapper.find('[test-update-notification]').vm.value).toBe(true);
+    expect(wrapper.vm.registration).toBe('Finished Loading');
+  });
+});
