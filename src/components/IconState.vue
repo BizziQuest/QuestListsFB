15b4403c00e10d3b57963d69bfb3d94ef9b83414
kickbuttowski80 <pazpaz25@gmail.com>
2021-08-13 18:54:10 -0700

    choose icon dialog will not close or choose on empty text field

--- src/components/IconState.vue
+++ src/components/IconState.vue
@@ -4,39 +4,35 @@
     <v-dialog v-model="dialog" max-width="400">
       <v-card>
         <v-card-title class="headline">Please choose your Icon</v-card-title>
-        <v-card-text>
-          <v-row>
-            <div class='pa-5 mb-5 rounded-t-xl' color="info">
-              Please enter the name of icon you wish to use for this state. You can get the icon
-              names from the following sites.<br/>
-            <v-chip
-              class="mr-2"
-              @click="openMaterial"
-            >
-              <v-icon class='mr-2'>mdi-vector-square</v-icon>
-              Material
-            </v-chip>
-            <v-chip
-              class="mr-2"
-              rounded
-              @click="openMaterialDesign">
-                <v-icon class='mr-2'>mdi-material-design</v-icon>
-                Material desgin
-            </v-chip>
-            </div>
-          </v-row>
-          <v-row>
-            <div class="local-icon">
-              <v-icon>{{localIcon}}</v-icon>
-            </div>
-            <v-text-field v-model="localIcon"></v-text-field>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="close">Nah...</v-btn>
-          <v-btn color="green darken-1" text @click="chooseIcon">Choose</v-btn>
-        </v-card-actions>
+        <v-form ref="iconForm">
+          <v-card-text>
+            <v-row>
+              <div class="pa-5 mb-5 rounded-t-xl" color="info">
+                Please enter the name of icon you wish to use for this state. You can get the icon names from the
+                following sites.<br />
+                <v-chip class="mr-2" @click="openMaterial">
+                  <v-icon class="mr-2">mdi-vector-square</v-icon>
+                  Material
+                </v-chip>
+                <v-chip class="mr-2" rounded @click="openMaterialDesign">
+                  <v-icon class="mr-2">mdi-material-design</v-icon>
+                  Material desgin
+                </v-chip>
+              </div>
+            </v-row>
+            <v-row>
+              <div class="local-icon">
+                <v-icon>{{ localIcon }}</v-icon>
+              </div>
+              <v-text-field v-model="localIcon" :rules="textFieldRules" required></v-text-field>
+            </v-row>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="close">Nah...</v-btn>
+            <v-btn color="green darken-1" text @click="chooseIcon">Choose</v-btn>
+          </v-card-actions>
+        </v-form>
       </v-card>
     </v-dialog>
   </span>
@@ -50,15 +46,18 @@ export default {
       localIcon: this.icon,
       dialog: false,
       previousIcon: this.icon,
+      textFieldRules: [(v) => !!v || 'icon name is required'],
     };
   },
   methods: {
     chooseIcon() {
+      if (this.$refs.iconForm.validate() === false) return;
       this.$emit('update:icon', this.localIcon);
       this.previousIcon = this.localIcon;
       this.dialog = false;
     },
     close() {
+      if (this.$refs.iconForm.validate() === false) return;
       this.localIcon = this.previousIcon;
       this.dialog = false;
     },
