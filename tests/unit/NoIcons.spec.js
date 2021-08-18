5a2eba13dde4e8613355809e588fe6beb95e6e92
kickbuttowski80 <pazpaz25@gmail.com>
2021-08-17 19:54:00 -0700

    fixed linter issues

--- tests/unit/NoIcons.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import ListItem from '@/components/ListItem.vue';
-
-describe('List Item.vue', () => {
-  test('no icons on empty input', () => {
-    const wrapper = shallowMount(ListItem);
-    
-  });
-});
