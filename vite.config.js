6aeba2eb7282b47d6899278ddbf0e167a7855b8f
Samuel C Tyler <skamansam@gmail.com>
2022-11-30 00:10:05 -0500

    add pwa plugin for vite; update deps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
+import { VitePWA } from 'vite-plugin-pwa';
+
+import * as vueConfig from './vue.config.js'
+
 const path = require('path');
+
+const pwaOptions = vueConfig.pwa;
+
 export default defineConfig({
   build: {
     cssCodeSplit: false,
@@ -14,7 +21,11 @@ export default defineConfig({
       // etc.
     ],
   },
-  plugins: [vue(),vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    VitePWA(pwaOptions)
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
