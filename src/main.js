de139cd22053be5e58a2c1ff3573c57f6c1dd9b6
Samuel C Tyler <skamansam@gmail.com>
2022-11-30 00:09:02 -0500

    remove custom service worker registration, for now.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { createApp } from 'vue';
 import App from './App.vue';
-import './registerServiceWorker';
+// import './registerServiceWorker';
 import router from './router';
 import store from './store';
 // import {setupI18n} from './i18n';
