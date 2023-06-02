9045736a709426cd00ad8dbdc42ba4dc27852743
Samuel C. Tyler <skamansam@gmail.com>
2023-06-02 00:13:45 -0400

    successfully parse and get suggestions from the algolia list instead of from algolia

--- src/components/DependencyEditor.vue
+++ src/components/DependencyEditor.vue
@@ -39,7 +39,8 @@ export default {
       equation: '',
       loading: false,
       equationSuggestions: [],
-      algoliaSearchClient: algolia
+      algoliaSearchClient: algolia,
+      autocompleteQuestList: null,
     };
   },
   mount() {
@@ -51,35 +52,70 @@ export default {
     // },
   },
   methods: {
-    findNextAutocomplete($event) {
+    parseEquationString() {
       const equationRegEx = /(?<line>(?<equation>(?<listNameQt>[\"\']?)(?<listName>.*?)\k<listNameQt>\.(?<itemNameQt>[\"\']?)(?<itemName>\w.*?)\k<itemNameQt>\s*(?<operator>[=!><]=?)\s*(?<stateValueQt>[\"\']?)(?<stateValue>\w.*?)\k<stateValueQt>(\s+(?<conjunction>and|or)\s+)?)+?)/ig;
       const matches = [equationRegEx.exec($event.target.value)];
+      debugger;
+      console.log('matches', matches);
       let match = null;
       while ((match = equationRegEx.exec($event.target.value)) != null) {
         matches.push(match);
       }
-      console.log('matches', matches.map(m => m.groups));
-      // matches now contains all the equations
+    },
+    async findNextAutocomplete($event) {
+      // NOTE: this means we only autocomplete for the last equation typed.
+      // one way to get around is to have different inputs for each equation.
       const equation = $event.target.value;
       const equations = equation.split(/\band\b|\bor\b/i); // Split by 'and' or 'or'
       const lastEquation = equations.at(-1);
       const [item, stateValue] = lastEquation.split(/==|!=|in|<|<=|>|>=/ig); // split by == != in < <= > >=
       const [listName, itemName] = item.split('.');
-      const equationSoFar = "";  // this should be filled in
-      if(stateValue) return this.getSuggestionsForWord(stateValue, 'stateGroup.states.text', equation);
-      if(itemName) return this.getSuggestionsForWord(itemName, 'listItems', listName);
-      return this.getSuggestionsForWord(listName, 'title', '');
+      console.log([listName, itemName, item, stateValue])
+      if(stateValue) {
+        const stateValuePivot = new RegExp(`${stateValue}$`);
+        const previousText = $event.target.value.split(stateValuePivot).slice(0,-1)
+        return this.getSuggestionsForStateValue(stateValue, previousText );
+      }
+      if(itemName !== undefined) {
+        console.log('itemName')
+        const itemNamePivot = new RegExp(`${itemName}$`);
+        const previousText = $event.target.value.split(itemNamePivot).slice(0,-1);
+        return this.getSuggestionsForItemName(itemName, previousText);
+      }
+      const listNamePivot = new RegExp(`${stateValue}$`);
+      const previousText = $event.target.value.split(listNamePivot).slice(0,-1)
+      const suggestions = await this.getSuggestionsForWord(listName, 'title', previousText);
+      this.autocompleteQuestList = suggestions?.[0];
+      return suggestions;
+
+    },
+    getSuggestionsForItemName(word, equation) {
+      if(word === '') {
+        this.equationSuggestions =  this.autocompleteQuestList.listItems.map(item => ({title: `${equation}"${item}"`}));
+        console.log(this.equationSuggestions)
+        return;
+      }
+
+      this.equationSuggestions = this.autocompleteQuestList.listItems.filter(listItem => {
+        if(listItem.toLowerCase().includes(word.toLowerCase())) return {title: `${equation}"${listItem}"`}
+      });
+      console.log(this.equationSuggestions)
+    },
+    getSuggestionsForStateValue(word, equation) {
+      this.equationSuggestions = this.autocompleteQuestList.stateGroup.states.filter(state => {
+        if(state.text.contains(word)) return {title: `${equation}"${listItem}"`}
+      });
     },
     async getSuggestionsForWord(word, field, equation) {
-      // we need to pass in the equation, up to this part.
-      // console.log(word, field, equation);
+
       const suggestions = await algoliaIndex.search(`${word}`, {
         hitsPerPage: 10,
         restrictSearchableAttributes: [field]
       });
-      console.log('suggestions', suggestions, suggestions.hits.length);
+      // console.log('suggestions', suggestions.hits.length, suggestions.hits.map(hit => ({title: `${equation}"${hit[field]}"`})));
 
-      this.equationSuggestions = suggestions.hits.map(hit => ({title: `${equation}"${hit.title}"`}));
+      this.equationSuggestions = suggestions.hits.map(hit => ({title: `${equation}"${hit[field]}"`}));
+      return suggestions.hits;
     },
   },
 };
