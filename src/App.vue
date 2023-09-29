ac0e2b5f6498b16d94895d340526e87a84ae5abe
Samuel C. Tyler <skamansam@gmail.com>
2023-09-29 00:07:12 -0400

    install @vueuse/head and setup main app with it. Fixes #221

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,15 @@ import BottomDrawerMenu from './components/Menus/BottomDrawerMenu.vue';
 import Notification from './components/GlobalNotification.vue';
 import appUpdate from './mixins/appUpdate';
 
+import Logo from './assets/logo.png';
+const siteData = {
+  name: 'QuestLists',
+  description: 'QuestLists is a simple app to keep track of your quests in your favorite games.',
+  author: 'QuestLists',
+  image: Logo,
+  imageAlt: 'QuestLists Logo',
+}
+
 export default {
   name: 'QuestLists App',
   components: {
@@ -77,6 +86,53 @@ export default {
       console.warn('Search Not Implemented');
     },
   },
+  head: {
+    title: 'QuestLists',
+    description: 'QuestLists is a simple app to keep track of your quests in your favorite games.',
+    author: 'QuestLists',
+    // Meta tags
+    meta: [
+      { name: 'application-name', content: siteData.name },
+      { name: 'description', content: siteData.description, id: 'desc' },
+      { name: 'author', content: siteData.author },
+
+      // Twitter
+      { name: 'twitter:title', content: siteData.name },
+      { name: 'twitter:description', c: siteData.description},
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: `@${siteData.author}` },
+      { name: 'twitter:creator', content: `@${siteData.author}` },
+      { name: 'twitter:image', content: siteData.image },
+      { name: 'twitter:image:alt', content: siteData.imageAlt },
+
+      // Google+ / Schema.org: https://schema.org/WebApplication
+      { itemprop: 'name', content: siteData.name },
+      { itemprop: 'description', content: siteData.description },
+      { itemprop: 'browserRequirements', content: 'requires evergreen browsers' },
+      { itemprop: 'applicationCategory', content: 'Lists' },
+
+      // Facebook / Open Graph
+      { property: 'fb:app_id', content: '123456789' },
+      { property: 'og:title', content: siteData.name },
+      { p: 'og:image', c: siteData.image },
+      { p: 'og:description', c: siteData.description },
+      { p: 'og:url', c: 'https://questlists.firebaseapp.com/' },
+      { p: 'og:site_name', c: siteData.name },
+      { p: 'og:type', c: 'website' },
+      { p: 'og:locale', c: 'en_US' },
+      { p: 'og:image:alt', c: siteData.imageAlt },
+    ],
+    // link tags
+    link: [
+      { rel: 'canonical', href: 'https://questlists.firebaseapp.com/', id: 'canonical' },
+      { rel: 'author', href: 'mailto:questlists@rbe.homeip.net', undo: false }, // undo property - not to remove the element
+      { rel: 'icon', href: './path/to/icon-16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    script: [
+      // { type: 'text/javascript', src: 'cdn/to/script.js', async: true, body: true}, // Insert in body
+      { t: 'application/ld+json', i: '{ "@context": "http://schema.org" }' },
+    ],
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import store from './store';
 import InstantSearch from 'vue-instantsearch/vue3/es';
 import vuetify from './plugins/vuetify';
 
+import { VueHeadMixin, createHead } from '@vueuse/head'
+const head = createHead()
+
+
 // we can use this if we need, but the reply theme preset uses a different font
 // import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import { auth } from './firebase';
@@ -29,12 +33,15 @@ onAuthStateChanged(auth, (user) => {
 // const i18n = setupI18n();
 
 const app = createApp(App)
+const headPlugin = createHead();
 
 app.use(vuetify);
 app.use(store);
 app.use(router);
 // app.use(i18n);
 app.use(InstantSearch);
+app.use(headPlugin);
+app.mixin(VueHeadMixin);
 
 if (window.Cypress) window.$app = app;
 
